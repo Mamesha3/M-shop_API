eb74f79ea91ef3e06ba069546967a737f8e46dd7
Mohammed Ahmed <webmame08@gmail.com>
2026-03-20 22:37:43 +0300

    integrate

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173'
+    origin: ['http://localhost:5173', 
+    `https://m-shop-food-delivery.netlify.app/`]
 }));
 
 // Routes
