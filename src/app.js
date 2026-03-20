2d9538cd2dd4fd832028181fa3317ef0b880caf2
Mohammed Ahmed <webmame08@gmail.com>
2026-03-20 22:40:39 +0300

    Fix CORS URL for Netlify frontend

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
     origin: ['http://localhost:5173', 
-    `https://m-shop-food-delivery.netlify.app/`]
+    'https://m-shop-food-delivery.netlify.app']
 }));
 
 // Routes
